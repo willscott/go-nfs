c2c78d18dfe422d76e8935c8b4bfb2862177c947
Cefan Rubin <cdrubin@alum.local>
2020-09-24 13:53:25 +0100

    add gitignore and fixed port

---
+++ example/aclunfs/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/willscott/memphis"
+
+	nfs "github.com/willscott/go-nfs"
+	nfshelper "github.com/willscott/go-nfs/helpers"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: aclunfs /path/to/folder\n")
+		return
+	}
+	listener, err := net.Listen("tcp", ":22049")
+	if err != nil {
+		fmt.Printf("Failed to listen: %v\n", err)
+		return
+	}
+	fmt.Printf("NFS server running at %s\n", listener.Addr())
+
+	fs := memphis.FromOS(os.Args[1])
+	bfs := fs.AsBillyFS(0, 0)
+
+	handler := nfshelper.NewNullAuthHandler(bfs)
+	cacheHelper := nfshelper.NewCachingHandler(handler)
+	fmt.Printf("%v", nfs.Serve(listener, cacheHelper))
+}
